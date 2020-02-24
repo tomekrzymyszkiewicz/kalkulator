fb293e1e2db11b1aea94cda142c9b6fce412d5f6
skeptical.knowledge <skeptical.knowledge@users.noreply.github.com>
2020-02-24 22:43:04 +0100

    Gotowa podstawowa mechanika

--- kalkulator/MainWindow.xaml.cs
+++ kalkulator/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace kalkulator
 {
@@ -14,17 +16,21 @@ namespace kalkulator
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-
+            Button b = (Button)sender;
+            poleTekstowe.Text += b.Content.ToString();
         }
 
         private void DEL_Click(object sender, RoutedEventArgs e)
         {
-
+            poleTekstowe.Text = "";
         }
 
         private void C_Click(object sender, RoutedEventArgs e)
         {
-
+            if (poleTekstowe.Text.Length > 0)
+            {
+                poleTekstowe.Text = poleTekstowe.Text.Substring(0, poleTekstowe.Text.Length - 1);
+            }
         }
 
         private void OFF_Click(object sender, RoutedEventArgs e)
@@ -34,7 +40,65 @@ namespace kalkulator
 
         private void Wynik_Click(object sender, RoutedEventArgs e)
         {
+            try
+            {
+                wynik();
+            }
+            catch
+            {
+                poleTekstowe.Text = "Error";
+            }
+        }
 
+        private void wynik()
+        {
+            String op; //operator
+            int iOp = 0; //pozycja operatora
+            if(poleTekstowe.Text.Contains("+"))
+            {
+                iOp = poleTekstowe.Text.IndexOf("+");
+            }
+            else if(poleTekstowe.Text.Contains("-"))
+            {
+                iOp = poleTekstowe.Text.IndexOf("-");
+
+            }
+            else if (poleTekstowe.Text.Contains("*"))
+            {
+                iOp = poleTekstowe.Text.IndexOf("*");
+
+            }
+            else if (poleTekstowe.Text.Contains("/"))
+            {
+                iOp = poleTekstowe.Text.IndexOf("/");
+            }
+            else
+            {
+                poleTekstowe.Text = "Error";
+            }
+
+            op = poleTekstowe.Text.Substring(iOp, 1);
+            double liczba1 = Convert.ToDouble(poleTekstowe.Text.Substring(0, iOp));
+            double liczba2 = Convert.ToDouble(poleTekstowe.Text.Substring(iOp + 1, poleTekstowe.Text.Length - iOp - 1));
+
+            switch (op)
+            {
+                case "+":
+                    poleTekstowe.Text += "=" + (liczba1 + liczba2);
+                    break;
+                case "-":
+                    poleTekstowe.Text += "=" + (liczba1 - liczba2);
+                    break;
+                case "*":
+                    poleTekstowe.Text += "=" + (liczba1 * liczba2);
+                    break;
+                case "/":
+                    poleTekstowe.Text += "=" + (liczba1 / liczba2);
+                    break;
+                default:
+                    poleTekstowe.Text = "Error";
+                    break;
+            }
         }
     }
 }
