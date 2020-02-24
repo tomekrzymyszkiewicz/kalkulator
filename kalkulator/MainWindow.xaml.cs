320f4efb2329f9274a35bdd76c24f0be4cce386f
skeptical.knowledge <skeptical.knowledge@users.noreply.github.com>
2020-02-24 20:44:56 +0100

    Utworzona siatka

--- kalkulator/MainWindow.xaml.cs
+++ kalkulator/MainWindow.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace kalkulator
 {
@@ -23,6 +10,31 @@ namespace kalkulator
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DEL_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void C_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OFF_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Wynik_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
